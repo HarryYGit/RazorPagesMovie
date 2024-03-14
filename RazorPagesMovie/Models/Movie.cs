f40f2f594516322a75bab2e410ffde297d85184c
Haoran Yan <84384076+HarryYGit@users.noreply.github.com>
2024-03-14 21:19:54 +1100

    Add a model: create a new folder "Models" within project create a new class 'Movie' within folder initial properties of the Movie:  Id, Title, ReleaseDate, Genre, Price

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; } // '?' question mark following a string indicates that the property is nullable
+
+        [DataType(DataType.Date)] // this attribute specifies the type of data in the ReleaseDate property. The user isn't required to enter time info in the date field; the time infor is not displayed, only date is.
+        public DateTime ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
